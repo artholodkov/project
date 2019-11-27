10c242128989bb956ee0fafb497057d5cea38df0
Artem <example@gmail.com>
2019-11-27 16:55:02 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+'use strict';
+a = 5;
+console.log(a);
+
+var number = 5;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log('string'*9);
+
+let something;
+console.log(something);
+
+let persone = {
+    name: "Jhon",
+    age: 25,
+    isMarried: false
+}
+
+console.log(persone["name"]);
+
+let arr = ['plum.pmg','orange.jpg','apple.bmp'];
+
+console.log(arr[0])
+
+//alert("Hello World");
+
+//let answer = +prompt("18 есть?", "Да");
+
+//console.log(typeof(answer));
+
+//console.log("arr" + " - object");
+//console.log(4 + +" - object");
+
+let incr = 10,
+    decr = 10;
+    
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2"===2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || isClose);
